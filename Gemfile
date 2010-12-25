71bf31514a9d24c6b6579dc8151f0c0a8b125313
Joe Van Dyk <joe@fixieconsulting.com>
2010-12-24 22:50:05 -0800

    Smushing thumbnails

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "http://rubygems.org"
 
 gem "sinatra", '1.1.0'
 gem "rmagick", '2.13.1'
+gem "smusher", '0.4.6'
--- image_monkey.rb
+++ image_monkey.rb
@@ -1,19 +1,19 @@
 require 'sinatra'
 require 'open-uri'
 require 'RMagick'
+require 'ftools'
+require 'smusher'
 
 module ImageMonkey
   SOURCE_HOST = "http://secure.tanga.com/"
 
   class Image
-    attr_reader :content_type
-
     def content_type
       @img.format
     end
 
-    def to_s
-      @img.to_blob
+    def thumbnail_path
+      @_path ||= "tmp/#{rand(100000000)}"
     end
 
     # Options: :width, :height, :path
@@ -21,8 +21,11 @@ module ImageMonkey
       file = open(SOURCE_HOST + options[:path])
       size = "#{options[:width]}x#{options[:height]}"
 
+      FileUtils.mkdir_p("tmp")
       @img = Magick::Image.from_blob(file.read).first
       @img.change_geometry(size) { |cols, rows, image| image.crop_resized!(cols, rows) }
+      @img.write(thumbnail_path)
+      Smusher.optimize_image(thumbnail_path)
     end
   end
 
@@ -31,7 +34,6 @@ end
 get '/*/*/*' do
   width, height, path = params[:splat]
   image = ImageMonkey::Image.new(:width => width, :height => height, :path => path)
-  expires 315360000, :public
-  content_type image.content_type
-  image.to_s
+  expires      315360000, :public
+  send_file    image.thumbnail_path, :type => image.content_type
 end
